f377461c1bcbb10719c1f10c66b3d71ccf6c97da
CryptoMatth <crypto.matth719@gmail.com>
2025-02-01 18:50:24 +0100

    Update search.js

--- search/search.js
+++ search/search.js
@@ -2,28 +2,11 @@
 document.addEventListener('DOMContentLoaded', function() {
     const searchInput = document.getElementById('search-input');
     const searchButton = document.getElementById('search-button');
-    const resultsContainer = document.getElementById('results-container');
 
     searchButton.addEventListener('click', function() {
         const query = searchInput.value.toLowerCase();
-        resultsContainer.innerHTML = '';
-
         if (query) {
-            fetch('search-index.json')
-                .then(response => response.json())
-                .then(data => {
-                    const results = data.filter(page => page.content.toLowerCase().includes(query));
-                    if (results.length > 0) {
-                        results.forEach(result => {
-                            const resultItem = document.createElement('div');
-                            resultItem.innerHTML = `<a href="${result.url}">${result.title}</a>`;
-                            resultsContainer.appendChild(resultItem);
-                        });
-                    } else {
-                        resultsContainer.innerHTML = '<p>Nessun risultato trovato.</p>';
-                    }
-                })
-                .catch(error => console.error('Errore nella ricerca:', error));
+            window.location.href = `search_results.html?query=${encodeURIComponent(query)}`;
         }
     });
 });
