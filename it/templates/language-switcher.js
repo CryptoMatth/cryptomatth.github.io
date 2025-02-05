67a9fa751c1d4b71b0ae62ba4689aa4a065f1d95
CryptoMatth <crypto.matth719@gmail.com>
2025-02-05 17:45:58 +0100

    Update language-switcher.js

--- it/templates/language-switcher.js
+++ it/templates/language-switcher.js
@@ -1,19 +1,23 @@
 // language-switcher.js
 document.addEventListener("DOMContentLoaded", function () {
-    document.getElementById('switch-to-it').addEventListener('click', function() {
+    document.getElementById('switch-to-it').addEventListener('click', function(event) {
+        event.preventDefault(); // Evita il comportamento predefinito del link
+        
         var currentPath = window.location.pathname;
         
-        // Se non siamo già sulla versione italiana, cambiamo l'URL
+        // Controlla se siamo già nella versione italiana
         if (!currentPath.includes('/it/')) {
             var newPath = currentPath.replace('/en/', '/it/');
             window.location.href = newPath;
         }
     });
 
-    document.getElementById('switch-to-en').addEventListener('click', function() {
+    document.getElementById('switch-to-en').addEventListener('click', function(event) {
+        event.preventDefault(); // Evita il comportamento predefinito del link
+        
         var currentPath = window.location.pathname;
 
-        // Se non siamo già sulla versione inglese, cambiamo l'URL
+        // Controlla se siamo già nella versione inglese
         if (!currentPath.includes('/en/')) {
             var newPath = currentPath.replace('/it/', '/en/');
             window.location.href = newPath;
