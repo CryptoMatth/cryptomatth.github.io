f86afd40d1603efbc82caee65c4685f0c009bfed
CryptoMatth <crypto.matth719@gmail.com>
2025-02-07 18:09:25 +0100

    Update url-handler.js

--- it/templates/url-handler.js
+++ it/templates/url-handler.js
@@ -2,20 +2,15 @@ document.addEventListener("DOMContentLoaded", function () {
     let host = window.location.host; // Es. cryptomatth.github.io
     let path = window.location.pathname; // Es. /it/crittografia/main_crittografia.html
 
-    console.log("Host: ", host);
-    console.log("Path: ", path);
-
     // Se il path non è pulito (ossia non contiene .html), lo correggiamo
     if (!path.endsWith(".html")) {
         let newPath = path + ".html"; // Aggiungiamo .html se non presente
-        console.log("Redirecting to: ", "https://" + host + newPath);
 
         // Cambia solo il pathname senza toccare l'host
         history.replaceState({}, "", "https://" + host + newPath);
     }
     // Se il percorso ha già .html, lo puliamo per rendere l'URL più bello
     let cleanPath = path.replace(/\.html$/, ""); // Rimuoviamo .html dal percorso
-    console.log("Cleaned URL: ", "https://" + host + cleanPath);
 
     // Cambia il pathname senza toccare l'host
     history.replaceState({}, "", "https://" + host + cleanPath);
