b772b16be6bb637cebb92a6539e52cca97450df8
CryptoMatth <crypto.matth719@gmail.com>
2025-02-27 00:09:29 +0100

    Update sidebar-toggle.js

--- general_templates/sidebar-toggle.js
+++ general_templates/sidebar-toggle.js
@@ -1,13 +1,21 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // Attendere che la sidebar sia caricata prima di collegare l'evento
     setTimeout(function () {
         const toggleButton = document.getElementById("toggle-sidebar");
         const sidebar = document.querySelector(".sidebar");
+        const overlay = document.createElement("div"); // Crea un overlay
+        overlay.classList.add("overlay");
+        document.body.appendChild(overlay); // Aggiungi l'overlay al body
 
         if (toggleButton && sidebar) {
             toggleButton.addEventListener("click", function () {
                 sidebar.classList.toggle("active");
+                overlay.classList.toggle("active");
+            });
+
+            overlay.addEventListener("click", function () {
+                sidebar.classList.remove("active");
+                overlay.classList.remove("active");
             });
         }
-    }, 500); // Aspetta mezzo secondo per garantire il caricamento dei template
+    }, 500); // Attendi che i template siano caricati
 });
